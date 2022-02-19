71715f7807a2c137551d9098904b0e929a41d299
Thefin145 <you@example.com>
2022-02-19 15:05:55 +0300

    внес корректировки

--- hi_23_1.py
+++ hi_23_1.py
@@ -1,17 +1,18 @@
 print('вывод таблицы умножения для чисел')
-q=1
+q = 1
 num1 = int(input('введите а= '))
 num2 = int(input('введите б= '))
 num3 = int(input('введите в= '))
 num4 = int(input('введите г= '))
-
-for j in range(num3, num4+q):
+num1 > 0
+num3 > 0
+num1 <= num2 <= 10
+num3 <= num4 <= 10
+for j in range(num3, num4 + q):
     print ('', j, end = '', sep = '\t')
-for i in range(num1, num2+q):
+for i in range(num1, num2 + q):
     print('\n', i, end = '')
-    for v in range(num3, num4+q):
-        print('\t', v*i, end = '')
-    
-    
+    for j in range(num3, num4 + q):
+        print('\t', j * i, end = '')           
 print('\n','нажмите Ввод для выхода', end = '')
 input()
