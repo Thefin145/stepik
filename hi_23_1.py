7bdf6130f0e710e2120e955bc808b6237665bcba
Thefin145 <you@example.com>
2022-02-20 20:58:48 +0300

    включил условия проверки

--- hi_23_1.py
+++ hi_23_1.py
@@ -1,18 +1,16 @@
-print('вывод таблицы умножения для чисел')
-q = 1
+print('вывод таблицы умножения для чисел от 1 до 10')
 num1 = int(input('введите а= '))
 num2 = int(input('введите б= '))
 num3 = int(input('введите в= '))
 num4 = int(input('введите г= '))
-num1 > 0
-num3 > 0
-num1 <= num2 <= 10
-num3 <= num4 <= 10
-for j in range(num3, num4 + q):
-    print ('', j, end = '', sep = '\t')
-for i in range(num1, num2 + q):
-    print('\n', i, end = '')
-    for j in range(num3, num4 + q):
-        print('\t', j * i, end = '')           
+if (num1 < 0 or num3 < 0) or (num2 > 10 or num4 > 10) or (num1 > num2 or num3 > num4):
+    print('заверщение программы')
+else:
+    for j in range(num3, num4 + 1):
+        print ('', j, end = '', sep = '\t')
+    for i in range(num1, num2 + 1):
+        print('\n', i, end = '')
+        for j in range(num3, num4 + 1):
+            print('\t', j * i, end = '')           
 print('\n','нажмите Ввод для выхода', end = '')
 input()
