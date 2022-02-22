5cb3b2431b9cc7ce004091dead00dfb75b181815
Thefin145 <you@example.com>
2022-02-22 22:58:27 +0300

    добавил пустую строку

--- hi_24_1.py
+++ hi_24_1.py
@@ -1,7 +1,5 @@
 print('определение % вхожднения в строку 2х заданных символов')
-
 gen = input('введите строку- ')
 nuc1 = input('введите искомый символ1- ')
 nuc2 = input('введите искомое символ2- ')
-
 print(float(gen.upper().count(nuc1) + gen.upper().count(nuc2)) / len(gen) * 100)
